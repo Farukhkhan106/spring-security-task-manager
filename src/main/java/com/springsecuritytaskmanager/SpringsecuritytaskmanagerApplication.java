874e77c57227696ab4313f8948a2c08db4fac952
Farukhkhan106 <farukhmansuri11122@gmail.com>
2026-05-06 21:31:46 +0530

    initial commit

---
+++ src/main/java/com/springsecuritytaskmanager/SpringsecuritytaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.springsecuritytaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritytaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritytaskmanagerApplication.class, args);
+    }
+
+}
